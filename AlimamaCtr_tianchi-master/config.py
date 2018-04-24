0131cb6dde173e2a3ca7f9fd67bf2a2be7da1f29
wanghaitao <wanghaitao@i.shu.edu.cn>
2018-04-24 10:18:06 +0800

    Signed-off-by: wanghaitao <wanghaitao@i.shu.edu.cn>

---
+++ AlimamaCtr_tianchi-master/config.py
@@ -0,0 +1,20 @@
+# origin train data & test data
+round1_train_file_path = './data/round1_ijcai_18_train_20180301.txt'
+round1_test_a_file_path = './data/round1_ijcai_18_test_a_20180301.txt'
+round1_test_b_file_path = './data/round1_ijcai_18_test_b_20180418.txt'
+
+# feature train data & test data
+train_valid_features_file_path = './temp/train_valid_features.csv'
+test_valid_features_file_path = './temp/test_valid_features.csv'
+train_data_features_file_path = './temp/train_data_features.csv'
+test_data_features_file_path = './temp/test_data_features.csv'
+
+train_valid_features_2_file_path = './temp/train_valid_features_2.csv'
+test_valid_features_2_file_path = './temp/test_valid_features_2.csv'
+train_data_features_2_file_path = './temp/train_data_features_2.csv'
+test_data_features_2_file_path = './temp/test_data_features_2.csv'
+# result data
+model_path = './result/model/model'
+model_train_log = 'train.log'
+model_valid_log = 'valid.log'
+result_filename = 'result.csv'
